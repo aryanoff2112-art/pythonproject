5cd948d0118572c4eb24ac2e21690de0208112ea
aryanoff2112-art <aryanoff2112@gmail.com>
2026-07-18 03:32:53 +0530

    modified:   password_manager.py modified:   passwords.txt new file:   salt.key

--- password_manager.py
+++ password_manager.py
@@ -1,29 +1,120 @@
-master_password = input("Enter your master password: ")
+import os
+import base64
+from cryptography.fernet import Fernet, InvalidToken
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+
+PASSWORD_FILE = "passwords.txt"
+SALT_FILE = "salt.key"
+
+def load_salt():
+    if not os.path.exists(SALT_FILE):
+        salt = os.urandom(16)
+        with open(SALT_FILE, "wb") as f:
+            f.write(salt)
+    else:
+        with open(SALT_FILE, "rb") as f:
+            salt = f.read()
+
+    return salt
+
+def generate_key(master_password):
+    salt = load_salt()
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
+
+    key = base64.urlsafe_b64encode(
+        kdf.derive(master_password.encode())
+    )
+
+    return Fernet(key)
+
+master_password = input("Enter Master Password: ")
+
+fer = generate_key(master_password)
+
 def add():
-    website = input("Enter the website: ")
-    user_name = input("Enter the Account name: ")
-    password = input("Enter the password: ")
-    with open("passwords.txt", "a") as f:
-        f.write(f"{website} | {user_name} | {password}\n")
-    print("Password stored successfully!")
+    website = input("Website: ").strip()
+    username = input("Username: ").strip()
+    password = input("Password: ").strip()
+
+    encrypted_password = fer.encrypt(password.encode()).decode()
+
+    with open(PASSWORD_FILE, "a") as f:
+        f.write(f"{website}|{username}|{encrypted_password}\n")
+
+    print("\nPassword stored successfully!\n")
 
 def view():
-     with open("passwords.txt", "r") as f:
-         for line in f:
-          print(line.rstrip())
-   
+
+    if not os.path.exists(PASSWORD_FILE):
+        print("\nNo passwords stored yet.\n")
+        return
+    try:
+        with open(PASSWORD_FILE, "r") as f:
+
+            found = False
+
+            for line in f:
+
+                line = line.strip()
+
+                if not line:
+                    continue
+
+                parts = line.split("|")
+
+                if len(parts) != 3:
+                    continue
+
+                website, username, encrypted = parts
+
+                try:
+                    password = fer.decrypt(encrypted.encode()).decode()
+
+                    print("-" * 45)
+                    print(f"Website : {website}")
+                    print(f"Username: {username}")
+                    print(f"Password: {password}")
+
+                    found = True
+
+                except InvalidToken:
+                    print("\nWrong Master Password!")
+                    return
+
+            if found:
+                print("-" * 45)
+            else:
+                print("No passwords found.")
+
+    except Exception as e:
+        print("Error:", e)
+
 while True:
-    mode = input("Choose mode (1: Store new Password, 2: View existing Passwords, q: Quit): ")
 
-    if mode == "1":
+    print("\nPASSWORD MANAGER")
+    print("1. Store New Password")
+    print("2. View Passwords")
+    print("Q. Quit")
+
+    choice = input("\nChoose an option: ").lower().strip()
+
+    if choice == "1":
         add()
-    elif mode == "2":
-        view()
-  
 
-    elif mode.lower() == "q":
-        print("Exiting the password manager. ")
+    elif choice == "2":
+        view()
+
+    elif choice == "q":
+        print("Goodbye!")
         break
 
     else:
-        print("Invalid mode selected.")
+        print("Invalid option.")
